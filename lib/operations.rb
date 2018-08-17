ccc7217668423e00ee22cc9606033f9d916e8eb7
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-17 15:51:48 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,13 +13,7 @@ end
 
 def not_safe?(speed)
   speed < 40 || speed > 60 ? true : false
- if speed > 60 
-  return true
-  elsif speed < 40
-  return true
-else
-  return false
-  speed = 50 ? true : false
+ 
 end
 end
 
